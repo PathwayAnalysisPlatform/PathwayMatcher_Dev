83500c0eeecd45870279482671df5df42db72943
Luis Francisco Hernandez Sanchez <luis.sanchez@uib.no>
2017-12-21 07:32:07 -0600

    Addded tests for genetic variants with chromosome and base pair

--- src/test/java/no/uib/pathwaymatcher/PathwayMatcherGeneticVariantsTest.java
+++ src/test/java/no/uib/pathwaymatcher/PathwayMatcherGeneticVariantsTest.java
@@ -42,9 +42,39 @@ class PathwayMatcherGeneticVariantsTest {
     }
 
     @Test
+    public void cysticFibrosisWithChrAndBpTest() {
+        String[] args = {"-t", "rsidList",
+                "-i", "src/main/resources/input/GeneticVariants/Chr_Bp/CysticFibrosis.txt",
+                "-tlp"};
+        PathwayMatcher.main(args);
+
+        //Check the output file
+        List<String> output = FileUtils.getInput("output.txt");
+        assertEquals(979, output.size());
+
+        List<String> statistics = FileUtils.getInput("pathwayStatistics.csv");
+        assertEquals(188, statistics.size());
+    }
+
+    @Test
     public void diabetesTest() {
         String[] args = {"-t", "rsidList",
                 "-i", "src/main/resources/input/GeneticVariants/RsId/Diabetes.txt",
+                "-tlp"};
+        PathwayMatcher.main(args);
+
+        //Check the output file
+        List<String> output = FileUtils.getInput("output.txt");
+        assertEquals(6417, output.size());
+
+        List<String> statistics = FileUtils.getInput("pathwayStatistics.csv");
+        assertEquals(677, statistics.size());
+    }
+
+    @Test
+    public void diabetesWithChrAndBpTest() {
+        String[] args = {"-t", "rsidList",
+                "-i", "src/main/resources/input/GeneticVariants/Chr_Bp/Diabetes.txt",
                 "-tlp"};
         PathwayMatcher.main(args);
 
@@ -79,6 +109,28 @@ class PathwayMatcherGeneticVariantsTest {
     }
 
     @Test
+    public void diabetesInYouthWithChrAndBpTest() {
+        // Execute the full pathway matcher
+        String[] args = {"-t", "rsidList",
+                "-i", "src/main/resources/input/GeneticVariants/Chr_Bp/DiabetesInYouth.txt"};
+        PathwayMatcher.main(args);
+
+        // Verify the selected preprocessor is correct
+        assertTrue(PathwayMatcher.preprocessor.getClass().equals(PreprocessorSnps.class));
+
+        // Verify the proteins mapped are correct
+        assertEquals(1, PathwayMatcher.entities.size());
+        assertTrue(PathwayMatcher.entities.contains(new Proteoform("Q9NQB0")));
+
+        //Check the output file
+        List<String> output = FileUtils.getInput("output.txt");
+        assertEquals(103, output.size());
+
+        List<String> statistics = FileUtils.getInput("pathwayStatistics.csv");
+        assertEquals(20, statistics.size());
+    }
+
+    @Test
     public void huntingtonsDiseaseTest() {
         String[] args = {"-t", "rsidList",
                 "-i", "src/main/resources/input/GeneticVariants/RsId/HuntingtonsDisease.txt"};
@@ -93,9 +145,45 @@ class PathwayMatcherGeneticVariantsTest {
     }
 
     @Test
+    public void huntingtonsDiseaseWithChrAndBpTest() {
+        String[] args = {"-t", "rsidList",
+                "-i", "src/main/resources/input/GeneticVariants/Chr_Bp/HuntingtonsDisease.txt"};
+        PathwayMatcher.main(args);
+
+        //Check the output file
+        List<String> output = FileUtils.getInput("output.txt");
+        assertEquals(350, output.size());
+
+        List<String> statistics = FileUtils.getInput("pathwayStatistics.csv");
+        assertEquals(114, statistics.size());
+    }
+
+    @Test
     public void HypoglycemiaTest() {
         String[] args = {"-t", "rsidList",
                 "-i", "src/main/resources/input/GeneticVariants/RsId/Hypoglycemia.txt"};
+        PathwayMatcher.main(args);
+
+        // Verify the selected preprocessor is correct
+        assertTrue(PathwayMatcher.preprocessor.getClass().equals(PreprocessorSnps.class));
+
+        // Verify the proteins mapped are correct
+        assertEquals(7, PathwayMatcher.entities.size());
+        assertTrue(PathwayMatcher.entities.contains(new Proteoform("P07550")));
+        assertTrue(PathwayMatcher.entities.contains(new Proteoform("P23786")));
+
+        //Check the output file
+        List<String> output = FileUtils.getInput("output.txt");
+        assertEquals(319, output.size());
+
+        List<String> statistics = FileUtils.getInput("pathwayStatistics.csv");
+        assertEquals(77, statistics.size());
+    }
+
+    @Test
+    public void HypoglycemiaWithChrAndBpTest() {
+        String[] args = {"-t", "rsidList",
+                "-i", "src/main/resources/input/GeneticVariants/Chr_Bp/Hypoglycemia.txt"};
         PathwayMatcher.main(args);
 
         // Verify the selected preprocessor is correct
@@ -139,6 +227,20 @@ class PathwayMatcherGeneticVariantsTest {
 
     @Test
     public void UlcerativeColitisTest() {
+        String[] args = {"-t", "rsidList",
+                "-i", "src/main/resources/input/GeneticVariants/RsId/UlcerativeColitis.txt"};
+        PathwayMatcher.main(args);
+
+        //Check the output file
+        List<String> output = FileUtils.getInput("output.txt");
+        assertEquals(7279, output.size());
+
+        List<String> statistics = FileUtils.getInput("pathwayStatistics.csv");
+        assertEquals(628, statistics.size());
+    }
+
+    @Test
+    public void UlcerativeColitisWithChrAndBpTest() {
         String[] args = {"-t", "rsidList",
                 "-i", "src/main/resources/input/GeneticVariants/RsId/UlcerativeColitis.txt"};
         PathwayMatcher.main(args);
